1f599a63539cc5e4daa81fbe1fbdf5a1765410cf
ssstumbles <sara.rosic@gmail.com>
2023-07-25 08:43:52 -0400

    user model added and tested

--- TJ/models.py
+++ TJ/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=15)
+    password = models.CharField()
+    first_name = models.CharField()
+    last_name = models.CharField()
+    email = models.EmailField()
+    img_url = models.TextField(null=True)
+
+    def __str__(self):
+        return self.username
