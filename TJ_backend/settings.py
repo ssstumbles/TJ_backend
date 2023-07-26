d495a2e3798a7d043a8e0ab9ba9156868c70f9c3
ssstumbles <sara.rosic@gmail.com>
2023-07-26 09:56:41 -0400

    models and REST done. Deployed to Elephant. Will be working in RReact now for the frontend and will hop back here when getting to axios/cors

--- TJ_backend/settings.py
+++ TJ_backend/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'TJ_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tj',
-        'USER': 'tjuser',
-        'PASSWORD': 'tj',
-        'HOST': 'localhost',
+        'NAME': 'yootkaqj',
+        'USER': 'yootkaqj',
+        'PASSWORD': 'k-HDt_Vm3hbwW-VCPYmBf7QsU3VH84Rz',
+        'HOST': '	hansken.db.elephantsql.com',
     }
 }
 
